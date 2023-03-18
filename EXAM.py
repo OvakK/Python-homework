5e77cfebb63bde83cfb0596861b80e77907e70ab
Ovak <kovak5063>
2023-03-18 13:24:49 +0400

    Exam

---
+++ EXAM.py
@@ -0,0 +1,48 @@
+# 1.SPLIT
+# def split(a):
+#     b = []
+#     spl = input('Vvedite razdelitel > ')
+#     d = ''
+#     for i in range(len(a)):
+#         if a[i] != spl:
+#             d += a[i]
+#         else:
+#             b.append(d)
+#             d = ""
+#     return b
+# print(split("asdhsjdfsdf.sdfsgsgfd.sgfgd.gdfg"))
+
+
+# 2.
+#
+# d = {'a': 3, 'b': 4, 'c': 5, 'd': 6}
+#
+# def dict_keys(x):
+#     return x.keys()
+#
+# print(dict_keys(d))
+#
+# Dictionary keys are always unic.
+
+# 3
+
+# lst = ['aaa', 'aaaa', 'ddddddd', 'qweety']
+
+###########################
+# a = []
+# for el in lst:
+#     if len(el) > 4:
+#         a.append(el)
+# print(a)
+
+############################
+
+# d = dict()
+# for el in lst:
+#     if len(el) > 4:
+#         d[el] = len(el)
+# print(d)
+
+##########################
+
+
